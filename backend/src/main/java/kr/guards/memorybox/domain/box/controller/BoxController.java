7f2b2b2ed2c6065e138bebf2b9f0bc65c8d8f392
강소현 <jayy_z@naver.com>
2022-04-19 14:18:05 +0900

    [S06P31E201-38] Feat : Backend Init

---
+++ backend/src/main/java/kr/guards/memorybox/domain/box/controller/BoxController.java
@@ -0,0 +1,11 @@
+package kr.guards.memorybox.domain.box.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/api/")
+public class BoxController {
+}
---
+++ backend/src/main/java/kr/guards/memorybox/domain/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package kr.guards.memorybox.domain.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/api/")
+public class UserController {
+}
