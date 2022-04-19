15fdaafda73163b26e1670b3aa1e5e82a2ba2d35
박동준 <qkrehdwns96@naver.com>
2022-04-19 16:19:49 +0900

    [S06P31E201-52] Feat : React-query Setting

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global';
 import theme from '../styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
+      <QueryClientProvider client={client}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
