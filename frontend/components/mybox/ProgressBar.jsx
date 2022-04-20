97929797786905255f62e8704c7687072f5f87aa
박동준 <qkrehdwns96@naver.com>
2022-04-20 13:53:31 +0900

    [S06P31E201-89] Design : Progressbar 완료

---
+++ frontend/components/mybox/ProgressBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ProgressBarContent } from './Mybox.style';
+
+const ProgressBar = () => {
+  return <ProgressBarContent />;
+};
+
+export default ProgressBar;
--- frontend/pages/mybox.jsx
+++ frontend/pages/mybox.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Box from '../components/mybox/Box';
 import { Wrapper, MapContainer } from '../styles/variables';
-import styled from 'styled-components';
 
 export default function mybox() {
   return (
     <MapContainer>
       <Wrapper>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
       </Wrapper>
     </MapContainer>
   );
