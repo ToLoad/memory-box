926d900922db0a6c6fef800866c831dc36bac3b9
박동준 <qkrehdwns96@naver.com>
2022-04-26 10:18:04 +0900

    [S06P31E201-145] Feat : Map Icon UI 및 폴더구조 변경

---
+++ frontend/components/Map/Map.style.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const MapModalWrapper = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const MapModalContent = styled(motion.div)`
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 15px;
+  bottom: 15px;
+  max-width: 600px;
+  height: 70vh;
+  background-color: yellow;
+  border-radius: 10px;
+  background-color: pink;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+  margin: auto;
+  @media ${props => props.theme.mobile} {
+    bottom: 0;
+    top: initial;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+  }
+`;
+
+const MapHeader = styled.div`
+  width: 95%;
+  padding: 2% 0;
+  margin: 0 auto;
+  height: 15%;
+  background-color: red;
+  text-align: center;
+  p {
+    font-size: 22px;
+    font-weight: 1000;
+  }
+`;
+
+const MapContent = styled.div`
+  width: 95%;
+  padding: 2% 0;
+  margin: 0 auto;
+  height: 70%;
+  background-color: yellow;
+`;
+
+const XIcon = styled.div`
+  position: absolute;
+  right: 20px;
+`;
+
+export { MapModalWrapper, MapModalContent, MapHeader, XIcon, MapContent };
--- frontend/components/mybox/ToggleButton.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-
-const ToggleButton = () => {
-  return <div>ToggleButton</div>;
-};
-
-export default ToggleButton;
