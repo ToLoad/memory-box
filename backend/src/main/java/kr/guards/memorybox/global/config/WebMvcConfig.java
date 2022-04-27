d698f10f55159c5f1623c293e68622c37eac4758
안영원 <kublaihkan@gmail.com>
2022-04-27 23:10:11 +0900

    Fix : CORS 처리를 위한 config 추가

---
+++ backend/src/main/java/kr/guards/memorybox/global/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package kr.guards.memorybox.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("X-AUTH-TOKEN")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000", "https://k6e201.p.ssafy.io");
+    }
+}
