a985e3698d8a7dc25abd70de2f302241e475f26d
박동준 <qkrehdwns96@naver.com>
2022-04-27 11:20:06 +0900

    [S06P31E201-153] Feat : 링크초대 유저 로그인페이지 완료

---
+++ frontend/components/login/JoinUserLogin.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { JoinUserWrapper, JoinUserContent } from './JoinUser.style';
+import KakaoLogin from './KakaoLogin';
+
+export default function JoinUserLogin() {
+  return (
+    <JoinUserWrapper>
+      <JoinUserContent>
+        <h1>로그인</h1>
+        <h2>부울경 2반의 추억여행</h2>
+        <div className="content">
+          <h3>
+            부울경 2반의 추억여행 타임캡슐입니다. 이 타임캡슐은 10년 뒤 open 될
+            예정입니다.
+          </h3>
+        </div>
+      </JoinUserContent>
+      <KakaoLogin />
+      <div style={{ marginBottom: '30px' }} />
+    </JoinUserWrapper>
+  );
+}
--- frontend/pages/mypage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MyPage from '../components/mypage/MyPage';
-
-export default function mypage() {
-  return <MyPage />;
-}
