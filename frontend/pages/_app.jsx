ddaad823d300d36bec987e3f7a8f3c6e466f235d
박동준 <qkrehdwns96@naver.com>
2022-04-28 23:47:35 +0900

    [S06P31E201-144] Feat : 시간대별 background 변경 완료

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -5,16 +5,38 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { Background } from '../styles/variables';
 import Navbar from '../components/Navbar/Navbar';
+import { useEffect, useRef, useState } from 'react';
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  const background = useRef();
+  const [todayhours, setTodayhours] = useState();
+  const [backgroundImg, setBackgroundImg] = useState();
+
+  useEffect(() => {
+    const today = new Date();
+    const hours = `0${today.getHours()}`.slice(-2);
+    setTodayhours(Number(hours));
+  });
+
+  useEffect(() => {
+    if (todayhours >= 18 || todayhours < 7) {
+      setBackgroundImg('Night');
+    } else {
+      setBackgroundImg('Day');
+    }
+  }, [todayhours]);
+
   return (
     <QueryClientProvider client={client}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <ReactQueryDevtools initialIsOpen={false} />
-        <Background />
+        <Background
+          ref={background}
+          className={backgroundImg === 'Day' ? 'day' : 'night'}
+        />
         <Navbar />
         <Component {...pageProps} />
       </ThemeProvider>
