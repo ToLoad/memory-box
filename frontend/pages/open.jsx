c19b3f607702f44575ede4d1e16824a8c337c45c
지수민 <wltn1873@gmail.com>
2022-04-28 10:25:22 +0900

    [S06P31E201-112] Feat : 상세보기UI 수정

--- frontend/pages/open.jsx
+++ frontend/pages/open.jsx
@@ -4,11 +4,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import {
   Wrapper,
-  Button,
   Header,
   SlickBlock,
   OpenCard,
 } from '../components/Slick/Slick.style';
+import { Button } from '../styles/variables';
 
 export default function open() {
   const count = 3;
@@ -26,75 +26,77 @@ export default function open() {
   };
   return (
     <Wrapper w="1000px">
-      <Header>
-        <div>대기중...</div>
-        <div>우리의 추억을 열기까지 {count}명이 필요해요</div>
-      </Header>
-      <SlickBlock>
-        <Slider {...settings}>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={1}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={0}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={0}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={0}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={0}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-          <OpenCard className="slick-card" come={0}>
-            <div>
-              <img src="혼구리2.png" alt="혼구리" />
-            </div>
-          </OpenCard>
-        </Slider>
-      </SlickBlock>
-      <Button>열기</Button>
+      <div>
+        <Header>
+          <div>대기중...</div>
+          <div>기억함을 열기까지 {count}명이 필요해요</div>
+        </Header>
+        <SlickBlock>
+          <Slider {...settings}>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={1}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={0}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={0}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={0}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={0}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+            <OpenCard className="slick-card" come={0}>
+              <div>
+                <img src="혼구리2.png" alt="혼구리" />
+              </div>
+            </OpenCard>
+          </Slider>
+        </SlickBlock>
+        <Button>기억함 열기</Button>
+      </div>
     </Wrapper>
   );
 }
