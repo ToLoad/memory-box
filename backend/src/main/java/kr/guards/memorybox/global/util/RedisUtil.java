df215b5177c160143c04037da095957170339b53
김지슬 <wltmfdl123@naver.com>
2022-04-29 09:49:42 +0900

    [S06P31E201-132] Feat : redis 토큰 보관용 util 생성

---
+++ backend/src/main/java/kr/guards/memorybox/global/util/RedisUtil.java
@@ -0,0 +1,35 @@
+package kr.guards.memorybox.global.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class RedisUtil {
+
+    @Value("${spring.security.jwt.refresh-token-expiration}")
+    private Integer refreshTokenExpiration;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public String getData(String key){
+        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setDataExpire(String key, String value){
+        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofMillis(refreshTokenExpiration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteData(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+}
