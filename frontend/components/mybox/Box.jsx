6713fae9dbb91ab83ae98b18b559d4a450a396db
박동준 <qkrehdwns96@naver.com>
2022-05-01 23:24:56 +0900

    [S06P31E201-169] Feat : 박스 상태(준비,열림,닫힘) 별 데이터 연동 및 상태 관리 완료

--- frontend/components/mybox/Box.jsx
+++ frontend/components/mybox/Box.jsx
@@ -9,75 +9,125 @@ import {
 } from './Mybox.style';
 
 import DdayButton from './DdayButton';
-import ProgressBar from './ProgressBar';
+import ProgressBar from '../Main/ProgressBar';
+import { IoIosArrowDown, IoIosArrowUp, IoMdAdds } from 'react-icons/io';
+import { MdMoreVert } from 'react-icons/md';
 
 const Box = props => {
-  const [boxHeight, setBoxHeight] = useState();
-  const Height = useRef();
+  const today = new Date();
+  const Dday = new Date(props.boxInfo.boxOpenAt);
+  const distance = Dday.getTime() - today.getTime();
+  const day = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const StartDay = new Date(props.boxInfo.boxCreatedAt);
+  const totalDayLenght = Dday.getTime() - StartDay.getTime();
+  const [userlength, setUserlength] = useState(false);
 
-  useEffect(() => {
-    setBoxHeight(Height.current.clientHeight);
-  }, []);
+  function getPercent() {
+    const StartDay = new Date(props.boxInfo.boxCreatedAt);
+    const Dday = new Date(props.boxInfo.boxOpenAt);
+    const today = new Date();
+    const distance = Dday.getTime() - today.getTime();
+    if (today.getTime() > Dday.getTime()) {
+      return 100;
+    }
+    const leftDay = ((totalDayLenght - distance) / totalDayLenght) * 100;
+    const leftDayPer = Math.floor(leftDay);
+    return leftDayPer;
+  }
+
+  function userSlice() {
+    if (props.boxInfo.user.length > 4) {
+      const userInfo = props.boxInfo.user.slice(0, 4);
+      return (
+        <>
+          <p style={{ marginRight: '5px' }}>...</p>
+          {userInfo.map((value, i) => {
+            return (
+              <>
+                {value.userProfileImage ? (
+                  <img className="userImage" src={value.userProfileImage} />
+                ) : (
+                  <img src="혼구리2.png" alt="사진없노" />
+                )}
+              </>
+            );
+          })}
+          <div className="plusButton"></div>
+        </>
+      );
+    } else {
+      return props.boxInfo.user.map((value, i) => {
+        return (
+          <>
+            {value.userProfileImage ? (
+              <img className="userImage" src={value.userProfileImage} />
+            ) : (
+              <img src="혼구리2.png" alt="사진없노" />
+            )}
+          </>
+        );
+      });
+    }
+  }
 
   return (
     <BoxWrapper
-      ref={Height}
       click={props.click}
-      Height={boxHeight}
       firstClick={props.firstClick}
+      num={props.num}
     >
       <div className={props.click ? 'off' : 'on'}>
         <ContentWrapper>
           <LeftContent />
           <RightContent>
             <div className="contentGroup">
-              <p>우리들의 추억여행</p>
-              <h4>내용</h4>
+              {/* <p>{props.boxInfo.boxName}</p> */}
+              <p>
+                으아아아아 으아아아아 으아아아아 으아아아아 으아아아아
+                으아아아아 으아아아아 으아아아아{' '}
+              </p>
+              {/* <h4>{props.boxInfo.boxDescription}</h4> */}
             </div>
             <div className="dayGroup">
               <div
                 className="toggleButton"
                 onClick={() => {
-                  props.set();
+                  props.set(props.num);
                 }}
               >
-                ++
+                <IoIosArrowDown />
+                {/* {click ? <IoIosArrowDown /> : <IoIosArrowUp />} */}
               </div>
               <div className="state">
-                <div>2022.05.27 </div>
-                <DdayButton />
+                <div>{props.boxInfo.boxOpenAt.slice(0, 10)}</div>
+                <DdayButton day={day} num={props.num} />
               </div>
-              <div className="user">
-                <img
-                  className="userImage"
-                  src="/assets/images/mobileNight.png"
-                  alt=""
-                />
-                <img
-                  className="userImage"
-                  src="/assets/images/mobileNight.png"
-                  alt=""
-                />
-                <img
-                  className="userImage"
-                  src="/assets/images/mobileNight.png"
-                  alt=""
-                />
-                <img
-                  className="userImage"
-                  src="/assets/images/mobileNight.png"
-                  alt=""
-                />
-              </div>
+              <div className="user">{userSlice()}</div>
             </div>
           </RightContent>
         </ContentWrapper>
-        <ProgressBarWrapper>
-          <ProgressBar />
-        </ProgressBarWrapper>
+        {/* <ProgressBarWrapper> */}
+        <ProgressBar percent={getPercent()} />
+        {/* </ProgressBarWrapper> */}
       </div>
     </BoxWrapper>
   );
 };
 
 export default Box;
+
+// console.log(props.boxInfo.boxOpenAt.slice(0, 4));
+//   const openYear = props.boxInfo.boxOpenAt.slice(0, 4);
+//   const openMonth = props.boxInfo.boxOpenAt.slice(5, 7);
+//   const openDay = props.boxInfo.boxOpenAt.slice(8, 10);
+//   console.log(openDay, openMonth, openYear);
+//   console.log(days, month, year);
+
+//   // 남은 day 계산. 열린 경우, 안열린 경우
+//   const leftYear = Number(openYear) - year;
+//   const leftMonth = Number(openMonth) - Number(month);
+//   const leftDays = Number(openDay) - Number(days);
+//   console.log(leftYear, leftMonth, leftDays);
+//   if (leftMonth < 0) {
+//     leftYear -= 1;
+//   }
