9d7aae548ceb7d01a9c0b8585450a8e742f30c13
안영원 <kublaihkan@gmail.com>
2022-05-12 10:48:16 +0900

    Fix : Remove Logs

--- backend/src/main/java/kr/guards/memorybox/global/auth/JwtAuthenticationFilter.java
+++ backend/src/main/java/kr/guards/memorybox/global/auth/JwtAuthenticationFilter.java
@@ -73,8 +73,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             try {
                 log.error("ACCESS-TOKEN" + accessToken);
                 // 2. Access Token에서 사용자 정보 추출
+                if (accessToken == null || accessToken.isEmpty()) throw new IllegalArgumentException("토큰이 존재하지 않습니다");
                 Long userSeq = jwtTokenUtil.getUserSeq(accessToken);
-                log.error("USERSEQ" + userSeq);
                 if (userSeq == null) {throw new IllegalArgumentException("정보가 담겨있지 않은 빈 토큰입니다.");}
 
                 // 3. Access Token 토큰에 포함된 유저 정보를 통해 실제 DB에 해당 정보의 계정이 있는지 조회
@@ -90,8 +90,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
                         // 4-2. jwt 토큰으로 부터 획득한 인증 정보(authentication) 설정
                         SecurityContextHolder.getContext().setAuthentication(jwtAuthentication);
-
-                        log.info("Security Filter - 로그인 한 유저 닉네임 : "+ user.getUserNickname());
                     }
                 } else {    // DB에 해당 유저 없는 경우
                     throw new NullPointerException("존재하지 않는 유저입니다.");
