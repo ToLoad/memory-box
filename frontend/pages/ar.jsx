7f2f74c39cd69f318cb624dd3916f9d0c63c88a2
김지슬 <wltmfdl123@naver.com>
2022-05-19 04:44:04 +0900

    [S06P31E201-327] Fix : 보물찾기 오류 수정 및 script tag 삭제

--- frontend/pages/ar.jsx
+++ frontend/pages/ar.jsx
@@ -110,12 +110,6 @@ export default function ar() {
 
   return (
     <>
-      <head>
-        <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
-        <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
-        <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
-        <script src="https://raw.githack.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
-      </head>
       {isOpen && (
         <Img src="/assets/images/6ob.gif" alt=""/>
       )}
