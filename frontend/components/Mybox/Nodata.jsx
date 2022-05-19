a90ed41349a9d627b07c7005d6b4eda1d1ed0809
박동준 <qkrehdwns96@naver.com>
2022-05-19 12:13:02 +0900

    [S06P31E201-337] Feat : 박스 리스트 페이지 배달완료 기억함이 없을 때 보물찾기 이동버튼 생성

--- frontend/components/Mybox/Nodata.jsx
+++ frontend/components/Mybox/Nodata.jsx
@@ -49,12 +49,42 @@ export default function Nodata(props) {
     Router.push('/create');
   }
 
+  function goToTreasure() {
+    Router.push('/treasure');
+  }
+
   function MakeText() {
     // 준비중인 기억함일때
     if (props.state === 'ready') {
-      return <h1>아직까지 만든 기억함이 없어요!!</h1>;
+      return (
+        <>
+          <h1>아직까지 만든 기억함이 없어요!!</h1>
+          <h2>추억을 보관하러 가볼까요?!</h2>
+          <div
+            className="button"
+            onClick={() => {
+              goToCreate();
+            }}
+          >
+            기억상자 만들러 가기
+          </div>
+        </>
+      );
     }
-    return <h1>아직 배달 완료된 기억함이 없어요!!</h1>;
+    return (
+      <>
+        <h1>아직까지 배달 완료된 기억함이 없어요!!</h1>
+        <h2>보물을 찾으러 떠나볼까요?</h2>
+        <div
+          className="button"
+          onClick={() => {
+            goToTreasure();
+          }}
+        >
+          보물찾으러 떠나기
+        </div>
+      </>
+    );
   }
 
   return (
@@ -62,15 +92,6 @@ export default function Nodata(props) {
       <NoDataContent>
         <h1>기억:함(函)</h1>
         {MakeText()}
-        <h2>추억을 보관하러 가볼까요?!</h2>
-        <div
-          className="button"
-          onClick={() => {
-            goToCreate();
-          }}
-        >
-          기억상자 만들러 가기
-        </div>
       </NoDataContent>
     </NoDataWrapper>
   );
